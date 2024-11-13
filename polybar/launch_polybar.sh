66c9a9bd648facf7a02801753c258b423ac28980
mLascurain <matiaslascurain04@gmail.com>
2024-11-13 11:31:52 -0300

    fixed second monitor polybar issue

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -1,6 +1,8 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    if [ "$m" != "HDMI-0" ]; then
+      MONITOR=$m polybar --reload toph &
+    fi
   done
 else
   polybar --reload toph &
